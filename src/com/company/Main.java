152d032c251e4a5d9b71c33b2dfde622853116a6
m1shakin <pavel.mishakin@mail.ru>
2021-10-13 11:02:22 +0300

    Init 228(2)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,50 +11,50 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Polygon polygon = new Polygon();
-        polygon.setTranslateY(0);
-        polygon.setTranslateX(-300);
-        polygon.getPoints().addAll(
-                650.0, 300.0,
-                500.0, 150.0,
-                350.0, 300.0);
-        polygon.setStroke(Color.BLACK);
-        polygon.setFill(Color.RED);
-
         Polygon polygon1 = new Polygon();
-        polygon1.setScaleX(2);
-        polygon1.setTranslateY(0);
-        polygon1.setTranslateX(300);
         polygon1.getPoints().addAll(
                 650.0, 300.0,
                 500.0, 150.0,
                 350.0, 300.0);
         polygon1.setStroke(Color.BLACK);
         polygon1.setFill(Color.RED);
+        polygon1.setTranslateY(0);
+        polygon1.setTranslateX(-300);
 
         Polygon polygon2 = new Polygon();
-        polygon2.setScaleY(2);
-        polygon2.setTranslateY(500);
-        polygon2.setTranslateX(-250);
         polygon2.getPoints().addAll(
                 650.0, 300.0,
                 500.0, 150.0,
                 350.0, 300.0);
         polygon2.setStroke(Color.BLACK);
         polygon2.setFill(Color.RED);
+        polygon2.setScaleX(1.5);
+        polygon2.setTranslateY(0);
+        polygon2.setTranslateX(300);
 
         Polygon polygon3 = new Polygon();
-        polygon3.setRotate(75);
-        polygon3.setTranslateY(500);
-        polygon3.setTranslateX(300);
         polygon3.getPoints().addAll(
                 650.0, 300.0,
                 500.0, 150.0,
                 350.0, 300.0);
         polygon3.setStroke(Color.BLACK);
         polygon3.setFill(Color.RED);
+        polygon3.setScaleY(1.5);
+        polygon3.setTranslateY(500);
+        polygon3.setTranslateX(-250);
 
-        Group root = new Group(polygon, polygon1, polygon2, polygon3);
+        Polygon polygon4 = new Polygon();
+        polygon4.getPoints().addAll(
+                650.0, 300.0,
+                500.0, 150.0,
+                350.0, 300.0);
+        polygon4.setStroke(Color.BLACK);
+        polygon4.setFill(Color.RED);
+        polygon4.setRotate(75);
+        polygon4.setTranslateY(500);
+        polygon4.setTranslateX(300);
+
+        Group root = new Group(polygon1, polygon2, polygon3, polygon4);
 
         Scene scene = new Scene(root, 1200, 900);
 
